154a50495e80d468c5f4f6798bd1f07e0644d62b
Micah Chambers (troller) <micahcc@ucla.edu>
2014-11-19 17:47:30 -0800

    general code clean up, better documentation and function splitting

--- nextfind.py
+++ nextfind.py
@@ -1,4 +1,4 @@
-#!/bin/python
+#!env python
 
 from sys import exit
 from subprocess import *
@@ -7,80 +7,113 @@ import re
 I3MSG = '/usr/bin/i3-msg'
 DMENU = '/usr/bin/dmenu'
 
+##
+# @brief Returns the giant list of windows in from i3
+#
+# @return
 def get_tree():
     p1 = Popen([I3MSG, "-t", "get_tree"], stdout=PIPE)
     i3input = str(p1.communicate()[0])
-    close_stdout(p1)
-    get_named_windows(i3input)
+    p1.stdout.close()
+    return i3input
 
-def get_named_windows(i3input):
+##
+# @brief Gets the list of named windows in stored as a name:[id, id...] dict
+#
+# @return Current active window (id), dictionary of windows name:[id, id, ...]
+def get_named_windows():
+
+    i3input = get_tree()
+
     count = 0
     current = -1
-    targets = dict()
+    windows = dict()
+    # ignore 0 width windows, save whether the image is focused
+    splitter = re.compile('([0-9]*).*focused":(false|true).*'\
+            '"geometry":{.*?"width":(?!0).*"name":"(.*?)"')
 
     sections = re.split('"id":', i3input);
     for idset in sections:
-	    results1 = re.search('([0-9]*).*focused":(false|true).*"geometry":{.*?"width":(?!0).*"name":"(.*?)"', idset)
-	    if results1 != None:
-		    if results1.group(2) == "true":
-			    current = int(results1.group(1))
+        results1 = splitter.search(idset)
+        if results1 != None:
+            if results1.group(2) == "true":
+                current = int(results1.group(1))
 
-		    if targets.get(results1.group(3)) == None:
-			    targets[results1.group(3)] = []
-		    targets[results1.group(3)].append(results1.group(1))
+            if windows.get(results1.group(3)) == None:
+                windows[results1.group(3)] = []
+            windows[results1.group(3).strip()].append(results1.group(1))
 
-    if len(targets) == 0:
-	    exit(1)
+    return (current, windows)
 
-    number_of_matching_window(i3input,targets,current)
 
-def number_of_matching_window(i3input,targets,current):
+##
+# @brief Open dmenu with the current set of all windows so that the user can
+# select one
+#
+# @param windows Dictionary of all windows with name : [ids...]
+# @param current ID of current window
+#
+# @return list of ids for the selected option
+def select_window(current, windows):
     #modify keys to indicate the number of matching windows
     options = dict()
-    for key in targets:
+    for key in windows:
         if __debug__:
             print(key)
 
-        newkey = "(" + str(len(targets[key])) + ") " + key
-        options[newkey] = targets[key]
+        newkey = "(" + str(len(windows[key])) + ") " + key
+        options[newkey] = windows[key]
 
     p1 = Popen([DMENU, '-i'], stdout=PIPE, stdin=PIPE)
     concat = '\n'.join(options.keys())
-    i3input = p1.communicate(bytes(concat,'UTF-8'))[0]
-    close_stdout(p1)
-    get_choice(current,i3input,options)
+    ret = p1.communicate(bytes(concat,'UTF-8'))[0].decode('UTF-8').rstrip()
+    p1.stdout.close()
 
-def get_choice(current,i3input,options):
-    key = i3input.decode('UTF-8').rstrip('\n');
-    if key == '':
-        exit(1)
+    selected = [int(i) for i in options[ret]]
+    return selected
 
-    if __debug__:
-        print(i3input, key, options[key])
-
+##
+# @brief Get the next selected window, cycling through if the current window is
+# a match and there are mutliple
+#
+# @param current ID of the currently focued window
+# @param selected Array of (int) ids for selected window(s)
+#
+# @return
+def cycle_selected(current, selected):
     #if the choice is a window type that has multiple values, then pick the next
     #from the current, in order of id
-    numtargs = [int(val) for val in options[key]]
-    if current in numtargs:
-        numtargs.sort();
-        pos = list.index(numtargs, current);
-        ii = (pos + 1)%len(numtargs)
+    if current in selected:
+        selected.sort();
+        pos = list.index(selected, current);
+        ii = (pos + 1)%len(selected)
     else:
         ii = 0
 
     if __debug__:
-        print (current,numtargs)
+        print (current,selected)
 
-    focus_window(ii,numtargs)
+    return selected[ii]
 
-def focus_window(ii,numtargs):
+##
+# @brief Focus the window with the specified id
+#
+# @param jumpid (int) id of window to jump to
+def focus_window(jumpid):
     if __debug__:
-        print([I3MSG, '[con_id="'+str(numtargs[ii])+'"] focus'])
+        print([I3MSG, '[con_id="%i"] focus'%jumpid])
 
-    p1 = Popen([I3MSG, '[con_id="'+str(numtargs[ii])+'"] focus'])
+    p1 = Popen([I3MSG, '[con_id="%i"] focus'%jumpid])
 
-def close_stdout(p1):
-    p1.stdout.close() # Allow p1 to receive a SIGPIPE if p2 exits
 
 if __name__ == '__main__':
-    get_tree()
+    current, windows = get_named_windows()
+    if not windows:
+        sys.exit(0)
+
+    selected = select_window(current, windows)
+    if not selected:
+        sys.exit(0)
+
+    next_selected = cycle_selected(current, selected)
+    focus_window(next_selected)
--- nextmatch.py
+++ nextmatch.py
@@ -1,48 +1,96 @@
-#!/bin/python
+#!env python
 
-from sys import argv, exit
+import sys
 from subprocess import *
 import re
 
 I3MSG = '/usr/bin/i3-msg';
 
+##
+# @brief Returns the tree of all windows
+#
+# @return Giant string with window properties
 def get_tree():
     p1 = Popen([I3MSG, "-t", "get_tree"], stdout=PIPE)
     i3input = str(p1.communicate()[0])
     p1.stdout.close()  # Allow p1 to receive a SIGPIPE if p2 exits.
-    search_window(i3input)
+    return i3input
 
-def search_window(i3input):
+##
+# @brief Search for the given expresssion in a name field
+#
+# @param i3input Raw tree information
+# @param expr Search expression (partial regex, matching name)
+#
+# @return pair of (current id, dictionary of matching windows)
+def search_window(i3input, expr):
     count   = 0
     current = -1
     targets = list()
+    split_re = re.compile('"id":')
 
-    sections = re.split('"id":', i3input);
+    # ignore 0 width windows, save whether the image is focused
+    search_re = re.compile('([0-9]*).*focused":(false|true).*'\
+            '"geometry":{.*?"width":(?!0).*"name":.*'+expr, \
+            flags=re.IGNORECASE)
+
+    sections = split_re.split(i3input);
     for idset in sections:
-        results1 = re.search('([0-9]*).*focused":(false|true).*"geometry":{.*?"width":(?!0).*"name":.*'\
-		    +argv[1], idset, flags=re.IGNORECASE)
+        results1 = search_re.search(idset)
         if results1 != None:
-	        targets.append(results1.group(1))
-	        if results1.group(2) == 'true':
-		        current = count
-	        count = count + 1
+            wid = int(results1.group(1))
+            targets.append(wid)
+            if results1.group(2) == 'true':
+                current = wid
+            count = count + 1
 
     #if no matchs were found, do nothing
-    print(targets)
-    print(current)
     if len(targets) == 0:
-        exit(1)
+        return (-1, [])
 
-    #sort the matching windows by id then go to the next if we are currently on one
-    targets = [int(val) for val in targets]
+    #sort the matching windows by id
     targets.sort()
-    focus_window(current,targets)
 
-def focus_window(current,targets):
-    print([I3MSG, '[con_id="'+str(targets[(current+1)%len(targets)])+'"] focus'])
-    p1 = Popen([I3MSG, '[con_id="'+str(targets[(current+1)%len(targets)])+'"] focus'])
+    return (current, targets)
+
+##
+# @brief Get the next selected window, cycling through if the current window is
+# a match and there are mutliple
+#
+# @param current ID of the currently focued window
+# @param selected Array of (int) ids for selected window(s)
+#
+# @return
+def cycle_selected(current, selected):
+    #if the choice is a window type that has multiple values, then pick the next
+    #from the current, in order of id
+    if current in selected:
+        selected.sort();
+        pos = list.index(selected, current);
+        ii = (pos + 1)%len(selected)
+    else:
+        ii = 0
+
+    if __debug__:
+        print (current,selected)
+
+    return selected[ii]
+
+# @brief Focus the window with the specified id
+#
+# @param jumpid (int) id of window to jump to
+def focus_window(jumpid):
+    if __debug__:
+        print([I3MSG, '[con_id="%i"] focus'%jumpid])
+
+    p1 = Popen([I3MSG, '[con_id="%i"] focus'%jumpid])
+
 
 if __name__ == '__main__':
-    if(len(argv) != 2):
+    if(len(sys.argv) != 2):
         exit(0)
-    get_tree()
+    i3input = get_tree()
+    current, matches = search_window(i3input, sys.argv[1])
+    next_selected = cycle_selected(current, matches)
+    focus_window(next_selected)
+
